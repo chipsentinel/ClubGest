b728622b6c7a53e6b4a01a836ecd3832d5e9fd75
chipsentinel <chipsentinel@gmail.com>
2025-12-06 19:52:18 +0100

    Fix: corrección de rutas del frontend y activación de CORS en server.js

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,12 @@ const cors = require('cors');           // Importar el módulo CORS para manejar
 const path = require('path');           // Importar el módulo Path para manejar rutas de archivos
 
 const app = express();                  // Crear una instancia de la aplicación Express 
-const PORT = process.env.PORT || 8080;  // Definir el puerto en el que el servidor escuchará
+app.use(cors());                        // Habilitar CORS para todas las rutas
+const PORT = 8080;                      // Definir el puerto en el que el servidor escuchará
 
+
+// Servir archivos estáticos del frontend
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 /*
 req: Objeto de solicitud que contiene información sobre la solicitud HTTP entrante.
@@ -21,13 +25,13 @@ res: Objeto de respuesta que se utiliza para enviar una respuesta al cliente.
 */
 
 // Definir una ruta para la raíz del servidor
-app.get('/frontend/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));                 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));            
 });
 
 // Definir una ruta para manejar solicitudes GET en /api/data
-app.get('/frontend/jugador', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'jugador.html'));
+app.get('/jugador', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'jugador.html'));
 });
 
 // Configurar middleware (intermediarios)  que procesan las solicitudes entrantes
@@ -45,6 +49,7 @@ cd .\backend\
 2. Instalar dependencias:
     npm init -y             // Inicializar un nuevo proyecto Node.js
     npm install express     // Instalar Express.js
+    npm install cors        // Instalar CORS
 3. Iniciar el servidor:
     node server.js          // Ejecutar el archivo server.js
 4. Verificar que el servidor esté corriendo:
